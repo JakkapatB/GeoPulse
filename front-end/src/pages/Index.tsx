cf9d822fde23a114e7827ef9d25b80cf9bde9056
Jakkapat <jakkapat.bo@kkumail.com>
2025-10-04 23:12:31 +0700

    [create] install ogl for use component in react bits galaxy for use in background

--- front-end/src/pages/Index.tsx
+++ front-end/src/pages/Index.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Galaxy from '../components/reactBits/Galaxy'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <div className='w-screen h-screen '>
+      <Galaxy
+        density={0.5}
+        glowIntensity={0.2}
+        saturation={0}
+        hueShift={150}
+        twinkleIntensity={0.3}
+        rotationSpeed={0.05}
+        repulsionStrength={1}
+        starSpeed={0.2}
+        speed={0.5}
+      />
+      <div className='absolute top-0 left-0 w-full flex justify-center items-center p-4'>
+      </div>
+    </div>
   )
 }
 
