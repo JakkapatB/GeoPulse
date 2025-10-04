7558d49d1264ae06b3f19668666c4d341e28159e
Jakkapat <jakkapat.bo@kkumail.com>
2025-10-04 22:45:49 +0700

    [init] use react router dom for route to pages

---
+++ front-end/src/routes/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import NotFound from '../pages/NotFound';
+import Index from '../pages/Index';
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    )
+}
+
+export default AppRoutes
