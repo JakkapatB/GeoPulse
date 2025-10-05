3a327ec2a265d1590ed90de27753a7b0ad8c152f
Jakkapat <jakkapat.bo@kkumail.com>
2025-10-05 14:45:38 +0700

    [update] in app routes have new paht for go to map monitor

--- front-end/src/routes/AppRoutes.tsx
+++ front-end/src/routes/AppRoutes.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
 import { Routes, Route } from "react-router-dom";
-import NotFound from '../pages/NotFound';
-import Index from '../pages/Index';
+import NotFound from "../pages/NotFound";
+import Index from "../pages/Index";
+import MainLayout from "../layouts/MainLayout";
+import MapMonitor from "../pages/MapMonitor";
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/map-monitor" element={<MainLayout />}>
+        <Route index element={<MapMonitor />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
