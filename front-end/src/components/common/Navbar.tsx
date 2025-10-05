93ed312205568ae3d8ea4c0ff8de4bb2982bb7db
Jakkapat <jakkapat.bo@kkumail.com>
2025-10-05 15:53:06 +0700

    [create] create navbar

---
+++ front-end/src/components/common/Navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return (
+    <nav className="flex flex-row items-center justify-between p-4 bg-background border-gray-300">
+    </nav>
+  )
+};
+
+export default Navbar;
--- front-end/src/layouts/MainLayout.tsx
+++ front-end/src/layouts/MainLayout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/common/Navbar";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-background text-text">
+      <header>
+        <Navbar />
+      </header>
       <main className="flex-1">
         <Outlet />
       </main>
