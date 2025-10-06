66be8feac20d64ecc4efc22c52f82acdae50f1ba
Jakkapat <jakkapat.bo@kkumail.com>
2025-10-07 01:37:54 +0700

    [update] page mapmonitor page can show map and data

--- front-end/src/pages/MapMonitor.tsx
+++ front-end/src/pages/MapMonitor.tsx
@@ -1,8 +1,38 @@
+import { useEffect, useState } from "react";
+import { getMapFeatures, type MapFeatureCollection } from "../services/mapApi";
+import MapView from "../components/Mapview";
 
 const MapMonitor = () => {
-  return (
-    <div>MapMonitor</div>
-  )
-}
+  const [data, setData] = useState<MapFeatureCollection | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-export default MapMonitor
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const result = await getMapFeatures("68db604f6d325faa74ba5bbd");
+        setData(result);
+        setError(null);
+      } catch (err) {
+        setError("Failed to fetch data");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <MapView
+        data={data as MapFeatureCollection}
+        loading={loading}
+        error={error}
+      />
+    </div>
+  );
+};
+
+export default MapMonitor;
